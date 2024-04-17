cb0df8e0ab84103c0c53878b0dad7aca639b50a3
DALARY <75627368+DALARY@users.noreply.github.com>
2024-04-17 15:55:01 +0200

    endpointUrl

--- src/Controller/BirthdayController.php
+++ src/Controller/BirthdayController.php
@@ -11,33 +11,47 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 #[Route("/api")]
 class BirthdayController extends AbstractController
 {
     #[Route('/birthday', name: 'app_birthday', methods: ['GET'])]
-    public function index(ManagerRegistry $doctrine, SerializerInterface $serializer): Response
+    public function index(ManagerRegistry $doctrine, SerializerInterface $serializer, UrlGeneratorInterface $urlGenerator): Response
     {
         $user = $this->getUser();
         if (!$user) {
             throw new AccessDeniedHttpException('Vous devez être connecté pour accéder à cette ressource.');
         }
         $birthdays = $doctrine->getRepository(Birthday::class)->findBy(['user' => $user]);
-        $serializerAll = $serializer->serialize($birthdays, 'json');
+        $endpointUrl = $urlGenerator->generate("app_birthday");
+
+        $arrayData = [
+            'birthdays' => $birthdays,
+            'endpointUrl' => $endpointUrl,
+        ];
+
+        $serializerAll = $serializer->serialize($arrayData, 'json');
         return new Response($serializerAll);
     }
 
     #[Route('/birthday/{id}', name: 'app_birthday_id', methods: ['GET'])]
-    public function getById(ManagerRegistry $doctrine, SerializerInterface $serializer, $id): Response
+    public function getById(ManagerRegistry $doctrine, SerializerInterface $serializer, $id, UrlGeneratorInterface $urlGenerator): Response
     {
         $user = $this->getUser();
         $birthday = $doctrine->getRepository(Birthday::class)->findOneBy(['user' => $user, 'id' => $id]);
-        $serializerAll = $serializer->serialize($birthday, 'json');
+        $endpointUrl = $urlGenerator->generate("app_birthday_id", ['id' => $id]);
+
+        $arrayData = [
+            'birthday' => $birthday,
+            'endpointUrl' => $endpointUrl,
+        ];
+        $serializerAll = $serializer->serialize($arrayData, 'json');
         return new Response($serializerAll);
     }
 
     #[Route('/birthday', name: 'app_birthday_add', methods: ['POST'])]
-    public function add(EntityManagerInterface $em, SerializerInterface $serializer, Request $request): Response
+    public function add(EntityManagerInterface $em, SerializerInterface $serializer, Request $request, UrlGeneratorInterface $urlGenerator): Response
     {
         $data = json_decode($request->getContent(), true);
         $user = $this->getUser();
@@ -50,13 +64,20 @@ class BirthdayController extends AbstractController
         $em->persist($birthday);
         $em->flush();
 
-        $jsonBirthday = $serializer->serialize($birthday, 'json');
+        $endpointUrl = $urlGenerator->generate("app_birthday_add");
+
+        $arrayData = [
+            'birthday' => $birthday,
+            'endpointUrl' => $endpointUrl,
+        ];
+
+        $jsonBirthday = $serializer->serialize($arrayData, 'json');
 
         return new Response($jsonBirthday);
     }
 
     #[Route('/birthday/{id}', name: 'app_birthday_edit', methods: ['PATCH'])]
-    public function edit(EntityManagerInterface $em, ManagerRegistry $doctrine, SerializerInterface $serializer, Request $request, $id): Response
+    public function edit(EntityManagerInterface $em, ManagerRegistry $doctrine, SerializerInterface $serializer, Request $request, $id, UrlGeneratorInterface $urlGenerator): Response
     {
         $birthday = $doctrine->getRepository(Birthday::class)->findOneBy(['id' => $id]);
         $user = $this->getUser();
@@ -76,19 +97,28 @@ class BirthdayController extends AbstractController
         $em->persist($birthday);
         $em->flush();
 
-        $jsonBirthday = $serializer->serialize($birthday, 'json');
+        $endpointUrl = $urlGenerator->generate("app_birthday_edit", ['id' => $id]);
+
+        $arrayData = [
+            'birthday' => $birthday,
+            'endpointUrl' => $endpointUrl,
+        ];
+
+        $jsonBirthday = $serializer->serialize($arrayData, 'json');
 
         return new Response($jsonBirthday);
     }
 
     #[Route('/birthday/{id}', name: 'app_birthday_delete', methods: ['DELETE'])]
-    public function delete(ManagerRegistry $doctrine, EntityManagerInterface $manager, $id): Response
+    public function delete(ManagerRegistry $doctrine, EntityManagerInterface $manager, $id, UrlGeneratorInterface $urlGenerator): Response
     {
         $user = $this->getUser();
         $birthday = $doctrine->getRepository(Birthday::class)->findOneBy(['user' => $user, 'id' => $id]);
         $manager->remove($birthday);
         $manager->flush();
 
-        return new Response("Suppression validé !");
+        $endpointUrl = $urlGenerator->generate("app_birthday_delete", ['id' => $id]);
+
+        return new Response("Suppression validé à cette url: ". $endpointUrl ." !");
     }
 }
