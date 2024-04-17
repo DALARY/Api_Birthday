869c0c0551c4d90e86c9dc8da7ddb486c52910e6
DALARY <75627368+DALARY@users.noreply.github.com>
2024-04-17 16:30:50 +0200

    Exception

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -1,16 +1,31 @@
 <?php
-
 namespace App\EventListener;
 
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
-final class ExceptionListener
+final class ExceptionListener implements EventSubscriberInterface
 {
+    public static function getSubscribedEvents(): array
+    {
+        // Définir les événements auxquels l'abonné souhaite souscrire
+        return [
+            KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+
     #[AsEventListener(event: KernelEvents::EXCEPTION)]
     public function onKernelException(ExceptionEvent $event): void
     {
-        // ...
+        $exception = $event->getThrowable();
+        $response = new JsonResponse([
+            'message' => $exception->getMessage(),
+            'code' => $exception->getCode(),
+        ]);
+
+        $event->setResponse($response);
     }
 }
